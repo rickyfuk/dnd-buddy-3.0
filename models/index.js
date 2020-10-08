9c95c320eecbc7da22885304b129d3f882faa071
digilau <rickyfukfuk@gmail.com>
2020-10-08 03:57:13 -0700

    update the dirname in models/index.js in order to locate config.js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 // const env = process.env.NODE_ENV || 'development';
 // const config = require(__dirname + '/../config/config.json')[env];
-const config = require('../config/config');
+const config = require(__dirname + '/../config/config');
 const db = {};
 
 // console.log(env);
