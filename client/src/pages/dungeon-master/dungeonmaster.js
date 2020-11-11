5ddb35efd0b445d81308e2d0fd0e276988c50b47
David Deaton <dideater@gmail.com>
2020-11-11 11:46:51 -0800

    working on dungeon master dice rolling feature

--- client/src/pages/dungeon-master/dungeonmaster.js
+++ client/src/pages/dungeon-master/dungeonmaster.js
@@ -7,12 +7,25 @@ import NavBot from '../../components/NavBottom/NavBot';
 
 
 function DungeonMaster() {
+
+  let small = Math.floor(Math.random()*200);
+
+  setInterval(function() {
+
+    const dice = 'df-d20-';
+
+    let num = Math.floor(Math.random() * 20 + 1);
+
+    const diceClassName = dice+num;
+
+  }, {small});
+
   return (
       <div className='body'>
         <NavTop />
         <div className='mainContainer'>
           <div>
-            <p className='bigCentered'><i className='df-d20-20'></i></p>
+            <p className='bigCentered'><i className={small}></i></p>
             <p className='lilCentered'>Under Construction</p>
           </div>
         </div>
