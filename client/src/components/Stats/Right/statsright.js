020fb7df415e930f2299f6e922bd4b31ea334ac8
David Deaton <dideater@gmail.com>
2020-11-17 23:40:50 -0800

    added stat functions based on class and race, minor css edits

--- client/src/components/Stats/Right/statsright.js
+++ client/src/components/Stats/Right/statsright.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import IntModal from './INTELLIGENCE/intModal';
+import IntStats from './INTELLIGENCE/intStats';
 import WisModal from './WISDOM/wisModal';
+import WisStats from './WISDOM/wisStats';
 import ChaModal from './CHARISMA/chaModal';
+import ChaStats from './CHARISMA/chaStats';
 import '../assets/stats.css';
 
 function StatsRight() {
@@ -10,38 +13,23 @@ function StatsRight() {
         <div>
 
             <div className="statBackground">
-                {/* <div className='statHelpButton'>
-                    <a className='fa fa-question-circle' href='/' id='str-guide' data-reveal-id='int-modal'> </a>
-                </div> */}
-                < IntModal/>
+                <IntModal/>
                 <div>
-                    <h2 className='sideStat' id='sideStatNumber2'>10</h2>
-                    <h2 className='statMod' id='mod2'>Mod +</h2>
-                    <h3 className='statTitle'>Intelligence</h3>
+                    <IntStats />
                 </div>
             </div>
 
             <div className="statBackground">
-                {/* <div className='statHelpButton'>
-                    <a className='fa fa-question-circle' href='/' id='str-guide' data-reveal-id='wis-modal'> </a>
-                </div> */}
                 < WisModal/>
                 <div>
-                    <h2 className='sideStat' id='sideStatNumber2'>10</h2>
-                    <h2 className='statMod' id='mod2'>Mod +</h2>
-                    <h3 className='statTitle'>Wisdom</h3>
+                    <WisStats />
                 </div>
             </div>
 
             <div className="statBackground">
-                {/* <div className='statHelpButton'>
-                    <a className='fa fa-question-circle' href='/' id='str-guide' data-reveal-id='cha-modal'> </a>
-                </div> */}
-                < ChaModal/>
+                <ChaModal />
                 <div>
-                    <h2 className='sideStat' id='sideStatNumber2'>10</h2>
-                    <h2 className='statMod' id='mod2'>Mod +</h2>
-                    <h3 className='statTitle'>Charisma</h3>
+                    <ChaStats />
                 </div>
             </div>
             
