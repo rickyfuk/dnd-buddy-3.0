a7b1e0d047c4c701963060bb3ec5e12971eaaf91
David Deaton <dideater@gmail.com>
2020-11-20 00:03:59 -0800

    updated saved character page to tie into backend

--- client/src/components/SavedCharacters/savedCharacters.js
+++ client/src/components/SavedCharacters/savedCharacters.js
@@ -4,13 +4,27 @@ import '../../pages/css/style.css';
 import './savedChar.css';
 import NavTop from '../NavTop/NavTop';
 import NavBot from '../NavBottom/NavBot';
-import SavedCharacterImage from './Saved-Components/savedImg';
-import SavedStatsBot from './Saved-Components/savedStatsBot';
-import SavedStatsTop from './Saved-Components/savedStatsTop';
-import SavedCounter from './Saved-Components/savedHP';
-import SavedBanner from './Saved-Components/savedNameBanner';
-import ArmorClass from './Saved-Components/savedAC';
-import SavedButtons from './Saved-Components/savedButtons';
+import SavedCharacterImageOne from './Saved-Components/Character-1/savedImg-1';
+import SavedStatsBotOne from './Saved-Components/Character-1/savedStatsBot-1';
+import SavedStatsTopOne from './Saved-Components/Character-1/savedStatsTop-1';
+import SavedCounterOne from './Saved-Components/Character-1/savedHP-1';
+import SavedBannerOne from './Saved-Components/Character-1/savedNameBanner-1';
+import ArmorClassOne from './Saved-Components/Character-1/savedAC-1';
+import SavedButtonsOne from './Saved-Components/Character-1/savedButtons-1';
+import SavedCharacterImageTwo from './Saved-Components/Character-2/savedImg-2';
+import SavedStatsBotTwo from './Saved-Components/Character-2/savedStatsBot-2';
+import SavedStatsTopTwo from './Saved-Components/Character-2/savedStatsTop-2';
+import SavedCounterTwo from './Saved-Components/Character-2/savedHP-2';
+import SavedBannerTwo from './Saved-Components/Character-2/savedNameBanner-2';
+import ArmorClassTwo from './Saved-Components/Character-2/savedAC-2';
+import SavedButtonsTwo from './Saved-Components/Character-2/savedButtons-2';
+import SavedCharacterImageThree from './Saved-Components/Character-3/savedImg-3';
+import SavedStatsBotThree from './Saved-Components/Character-3/savedStatsBot-3';
+import SavedStatsTopThree from './Saved-Components/Character-3/savedStatsTop-3';
+import SavedCounterThree from './Saved-Components/Character-3/savedHP-3';
+import SavedBannerThree from './Saved-Components/Character-3/savedNameBanner-3';
+import ArmorClassThree from './Saved-Components/Character-3/savedAC-3';
+import SavedButtonsThree from './Saved-Components/Character-3/savedButtons-3';
 
 function SavedCharacter() {
 
@@ -21,57 +35,57 @@ function SavedCharacter() {
             <div className='rowOne'>
                 <div className='group'>
                     <div className='columnOne'>
-                        <SavedCharacterImage />
+                        <SavedCharacterImageOne />
                     </div>
                     <div className='columnTwo'>
-                        <SavedBanner />
-                        <SavedButtons />
+                        <SavedBannerOne />
+                        <SavedButtonsOne />
                     </div>
                     <div className='column3'>
-                        <SavedStatsTop />
-                        <SavedStatsBot />
+                        <SavedStatsTopOne />
+                        <SavedStatsBotOne />
                     </div>
                     <div className='columnFour'>
-                        <SavedCounter />
-                        <ArmorClass />
+                        <SavedCounterOne />
+                        <ArmorClassOne />
                     </div>
                 </div>
             </div>
             <div className='rowOne'>
                 <div className='group'>
                     <div className='columnOne'>
-                        <SavedCharacterImage />
+                        <SavedCharacterImageTwo />
                     </div>
                     <div className='columnTwo'>
-                        <SavedBanner />
-                        <SavedButtons />
+                        <SavedBannerTwo />
+                        <SavedButtonsTwo />
                     </div>
                     <div className='column3'>
-                        <SavedStatsTop />
-                        <SavedStatsBot />
+                        <SavedStatsTopTwo />
+                        <SavedStatsBotTwo />
                     </div>
                     <div className='columnFour'>
-                        <SavedCounter />
-                        <ArmorClass />
+                        <SavedCounterTwo />
+                        <ArmorClassTwo />
                     </div>
                 </div>
             </div>
             <div className='rowOne'>
                 <div className='group'>
                     <div className='columnOne'>
-                        <SavedCharacterImage />
+                        <SavedCharacterImageThree />
                     </div>
                     <div className='columnTwo'>
-                        <SavedBanner />
-                        <SavedButtons />
+                        <SavedBannerThree />
+                        <SavedButtonsThree />
                     </div>
                     <div className='column3'>
-                        <SavedStatsTop />
-                        <SavedStatsBot />
+                        <SavedStatsTopThree />
+                        <SavedStatsBotThree />
                     </div>
                     <div className='columnFour'>
-                        <SavedCounter />
-                        <ArmorClass />
+                        <SavedCounterThree />
+                        <ArmorClassThree />
                     </div>
                 </div>
             </div>
