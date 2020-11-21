38da5b2e7438b2b29bf2e090e5c8d52ea56c6f44
David Deaton <dideater@gmail.com>
2020-11-21 05:19:03 -0800

    main image generation

--- client/src/components/NavBottom/NavBot.js
+++ client/src/components/NavBottom/NavBot.js
@@ -6,13 +6,16 @@ import DiceRoll8 from '../Dice/dice8Modal';
 import DiceRoll10 from '../Dice/dice10Modal';
 import DiceRoll12 from '../Dice/dice12Modal';
 import DiceRoll20 from '../Dice/dice20Modal';
-
+import useSound from 'use-sound';
+import DiceSound from '../Dice/dicesound.mp3';
 
 function NavBot() {
 
+  const [play] = useSound(DiceSound);
+
   return (
 
-    <nav className='navbarBottom navbar dice-nav fixed-bottom navbar-light'>
+    <nav className='navbarBottom navbar dice-nav fixed-bottom navbar-light' onClick={play}>
       <DiceRoll4 />
       <DiceRoll6 />
       <DiceRoll8 />
