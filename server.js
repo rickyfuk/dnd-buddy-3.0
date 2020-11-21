228333d6d9100512c0407eea823d3864c5e0c6e7
digilau <rickyfukfuk@gmail.com>
2020-11-21 04:48:52 -0800

    update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// Main server
+// Main
 // Get the express dependencies
 const express = require('express');
 // express-session for controlling the validality of the login session
 const session = require('express-session');
 
-const dbConnection = require('./database'); 
+const dbConnection = require('./database');
 const MongoStore = require('connect-mongo')(session);
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 // Requiring passport from the passport-config.js
 // (no need for require passport directly as the module export by passport-config has included passport module)
 const passport = require('./passport/index');
@@ -28,22 +28,22 @@ const db = require('./models');
 require('dotenv').config();
 
 // MIDDLEWARE
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(
 	bodyParser.urlencoded({
-		extended: false
+		extended: false,
 	})
-)
-app.use(bodyParser.json())
+);
+app.use(bodyParser.json());
 
 // Sets up the Express app to handle data parsing
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
 // Static directory
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-  }
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
 
 // (standard requirment for using passport module)
 // the sessions moduel for keeping track of our user's login status
@@ -62,7 +62,7 @@ app.use(
 		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
 		store: new MongoStore({ mongooseConnection: dbConnection }),
 		resave: false, //required
-		saveUninitialized: false //required
+		saveUninitialized: false, //required
 	})
 );
 app.use(passport.initialize());
@@ -72,18 +72,14 @@ app.use(passport.session());
 // app.use(routes);
 app.use('/user', user);
 
-
 // Set the route
 // const htmlRoute = require('./controllers/htmlRoute');
 // const apiRoute = require('./controllers/apiRoute');
 
 // app.use(apiRoute);
 
-
-
 // console.log(routes);
 // console.log(apiRoute);
-
 
 // app.get("*", function(req, res) {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
@@ -91,7 +87,6 @@ app.use('/user', user);
 // console.log(process.env.JAWSUSERNAME);
 // console.log(process.env.JAWSPASSWORD);
 // console.log(process.env.DATABASE);
-
 
 // sync the sequelize model
 // 1. force has set to "false" to stop dropping the table when the server restart
@@ -101,6 +96,6 @@ app.use('/user', user);
 // 	});
 // });
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
 	console.log('App listening on PORT ' + PORT);
 });
